96d2d22adb1eca85ba36372e8546c7e3796a9b29
sowmyay <sowmyayellapragada@gmail.com>
2024-02-04 17:41:50 +0100

    Open mail client on contact form submission

--- assets/js/form-handler.js
+++ assets/js/form-handler.js
@@ -18,24 +18,28 @@ window.addEventListener("DOMContentLoaded", function () {
     form.addEventListener("submit", function (ev) {
       ev.preventDefault();
       var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
+      console.log(data)
+      console.log(data.get("Name"))
+      window.location.href = 'mailto:sowmyayellapragada@gmail.com?subject=New contact request from - ' + data.get("Name") + ' (' + data.get("email") + ')' + '&body=' + data.get("message");
+//      ajax(form.method, "email.php", data, success, error);
     });
   }
 });
 
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
+//// helper function for sending an AJAX request - not used
+//
+//function ajax(method, url, data, success, error) {
+//  var xhr = new XMLHttpRequest();
+//  xhr.open(method, url);
+//  console.log(url)
+//  xhr.setRequestHeader("Accept", "application/json");
+//  xhr.onreadystatechange = function () {
+//    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//    if (xhr.status === 200) {
+//      success(xhr.response, xhr.responseType);
+//    } else {
+//      error(xhr.status, xhr.response, xhr.responseType);
+//    }
+//  };
+//  xhr.send(data);
+//}
